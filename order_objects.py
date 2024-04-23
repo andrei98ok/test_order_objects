b50010042f035491c8a5262fc3e8983f9f628fb1
andrei98ok <bestik2.0@gmail.com>
2024-04-23 22:53:37 +0700

    Новая версия с разделением на функции

--- order_objects.py
+++ order_objects.py
@@ -1,17 +1,17 @@
-def order_objects_by_color(objects):
-    color_mapping = {'К': 3, 'З': 1, 'С': 2}  # Задаем порядок цветов
-    sorted_objects = sorted(objects, key=lambda x: color_mapping.get(x, 0))  # Сортируем объекты по порядку цветов
-    return sorted_objects
-
-while True:
-    # Запрос ввода объектов у пользователя
-    input_objects = input("Введите список объектов (буквы С З К без разделителей): ").strip()
-
+def order_objects_by_color(input_objects):
     # Преобразуем все буквы в верхний регистр и фильтруем только С, З и К
     filtered_input = ''.join(filter(lambda x: x in ['С', 'З', 'К'], input_objects.upper()))
 
     # Разделяем отфильтрованные буквы на отдельные объекты
     input_objects = list(filtered_input)
+
+    color_mapping = {'К': 3, 'З': 1, 'С': 2}  # Задаем порядок цветов
+    sorted_objects = sorted(input_objects, key=lambda x: color_mapping.get(x, 0))  # Сортируем объекты по порядку цветов
+    return sorted_objects
+
+while True:
+    # Запрос ввода объектов у пользователя
+    input_objects = input("Введите список объектов (буквы С З К без разделителей): ").strip()
 
     # Вызываем функцию
     output_objects = order_objects_by_color(input_objects)
